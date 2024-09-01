c47261ce5bb25edca825982cdaddb6fba3e1e754
Luc4sL1 <lucaslyma.0606@gmail.com>
2024-09-01 20:10:05 -0300

    Tela-Login-Inspirart

--- Inspirart/src/App.jsx
+++ Inspirart/src/App.jsx
@@ -1,13 +1,14 @@
 
+import './App.css'
+import Login from './components/Login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+         <Login/>
+    </div>
+  );
 }
 
 export default App
